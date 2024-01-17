c81c25ad4413634e30b5871b9ee4cf8d19c81b25
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-01-18 00:29:46 +0530

    Rainbowkit: Added coolMode animation prop

--- src/utils/wagmiprovider.tsx
+++ src/utils/wagmiprovider.tsx
@@ -110,6 +110,7 @@ function WagmiProvider(props: any) {
           <RainbowKitProvider
             chains={chains}
             theme={darkTheme({ accentColor: "#7b3fe4" })}
+            coolMode
           >
             {props.children}
           </RainbowKitProvider>
